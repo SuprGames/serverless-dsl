13f2bc35c2e71c01a76a3d11636f405faffff946
Diego Marzo <diegomarzo@gmail.com>
2020-08-21 14:43:36 +0200

    Clean up of Gradle File

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
-import com.github.jengelman.gradle.plugins.shadow.transformers.Log4j2PluginsCacheFileTransformer
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     kotlin("jvm") version "1.3.71"
-    id("com.github.johnrengelman.shadow") version ("6.0.0")
     `maven-publish`
 }
 
@@ -16,16 +13,8 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-tasks.withType<ShadowJar> {
-    transform(Log4j2PluginsCacheFileTransformer::class.java)
-}
-
 repositories {
     mavenCentral()
-}
-
-tasks.build {
-    finalizedBy(getTasksByName("shadowJar", false))
 }
 
 dependencies {
