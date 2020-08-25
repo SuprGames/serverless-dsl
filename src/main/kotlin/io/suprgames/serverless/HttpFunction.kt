6a1266ab8b4b2b9f48f4b9eef048cdc6ef4537c0
Diego Marzo <diegomarzo@gmail.com>
2020-08-25 23:17:24 +0200

    Adding authorizer to HttpFunction

--- src/main/kotlin/io/suprgames/serverless/HttpFunction.kt
+++ src/main/kotlin/io/suprgames/serverless/HttpFunction.kt
@@ -1,3 +1,7 @@
 package io.suprgames.serverless
 
-annotation class HttpFunction(val name: String = "", val method: HttpMethod = HttpMethod.GET, val path: String)
+annotation class HttpFunction(val name: String = "",
+                              val method: HttpMethod = HttpMethod.GET,
+                              val path: String,
+                              val cors: Boolean = true,
+                              val authorizer: String = "")
